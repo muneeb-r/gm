98157fa10a793c3c402e8aa1665ceaea74a0121b
flask <muneebjs.css@gmail.com>
2023-02-23 20:08:46 +0500

    first commit

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,7 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import Student from "@/models/student"
-
 export default async function handler(req, res) {
   res.status(200).json({hello:'hello'})
 }
