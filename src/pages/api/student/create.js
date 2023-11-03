06298878ed1a4b9a24a938bd2cc6180beb9ffd40
Muneeb Rana <muneebjs.css@gmail.com>
2023-11-03 11:07:20 +0500

    as

--- src/pages/api/student/create.js
+++ src/pages/api/student/create.js
@@ -2,13 +2,16 @@ import connectDB from "@/middleware/mongoos"
 import Student from "@/models/student"
 
 async function handler(req, res) {
+    try {
+        if (!req.method === 'POST') return
 
-    if (!req.method === 'POST') return
+        const rn = await Student.countDocuments() + 1
+        const stu = await Student.create({ ...req.body, rn })
 
-    const rn = await Student.countDocuments({campus: req.body.campus})+1
-    const stu = await Student.create({ ...req.body, rn })
-
-    res.status(201).json(stu)
+        res.status(201).json(stu)
+    } catch (e) {
+        res.status(500).json({message:'Internal server error', e})
+    }
 
 }
 
